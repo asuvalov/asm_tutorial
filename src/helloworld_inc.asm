f3a868c4992599a7d8e20f4095a427ce1356ca10
Andrey Suvalov <suvalov.andrew@gmail.com>
2020-01-01 00:21:18 +0300

    L5. External files

---
+++ src/helloworld_inc.asm
@@ -0,0 +1,17 @@
+%include 'functions.asm'
+
+SECTION .data
+msg1    db      'Hello brave new world', 0Ah
+msg2    db      'This is how we recycle in NASM.', 0Ah
+
+SECTION .text
+global _start
+_start:
+    mov     eax, msg1
+    call    printstr
+
+    mov     eax, msg2
+    call    printstr
+
+    call    exit
+
