052228d28349cf836d370cf3a67b55c30f3a1c22
Andrey Suvalov <suvalov.andrew@gmail.com>
2019-12-31 23:40:56 +0300

    L4. Subroutines

--- src/helloworld_len.asm
+++ src/helloworld_len.asm
@@ -5,17 +5,9 @@ SECTION .text
 global _start
 
 _start:
-    mov     ebx, msg
-    mov     eax, ebx
-
-nextchar:
-    cmp     byte [eax], 0   ; set zero flag
-    jz      finished        ; jump if ZF true
-    inc     eax             ; increment
-    jmp     nextchar
-
-finished:
-    sub     eax, ebx        ; eax - ebx = strlen
+    mov     eax, msg
+    call    strlen
+    
     mov     edx, eax
     mov     ecx, msg
     mov     ebx, 1
@@ -25,4 +17,19 @@ finished:
     mov     ebx, 0
     mov     eax, 1
     int     80h
+
+strlen:
+    push    ebx
+    mov     ebx, eax
+
+nextchar:
+    cmp     byte [eax], 0   ; set zero flag
+    jz      finished        ; jump if ZF true
+    inc     eax             ; increment
+    jmp     nextchar
+
+finished:
+    sub     eax, ebx
+    pop     ebx
+    ret
 
